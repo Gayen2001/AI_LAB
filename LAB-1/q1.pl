1cd0e2a495d12dbac469d4f5ed45772e3bcdd34c
Gayen2001 <gayenpartha55@gmail.com>
2024-09-09 11:50:32 +0530

    ASSIMENT

---
+++ LAB-1/q1.pl
@@ -0,0 +1,14 @@
+% Q1:getting familiar with prolog (English sentence to fact conversion)
+    % 1.Gold is valuable.
+    % 2.Ram is happy 
+    % 3.Sita is happy.
+% Code:
+valuable(gold).
+happy(sita).
+happy(ram).
+
+
+
+% Quarry:
+% happy(X).
+% valuable(X).
